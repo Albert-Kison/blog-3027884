8facd2ac93d70c07de84683880456e36a2e01811
Albert Kison <albert.kison@student.griffith.ie>
2024-06-12 22:12:30 +0100

    Change to deploy

--- config/database.js
+++ config/database.js
@@ -1,6 +1,8 @@
 module.exports = {
   host: "eu-cluster-west-01.k8s.cleardb.net",
   user: "bfe3b37575de05",
-  password: "c82fda53!",
+  password: "c82fda53",
   database: "heroku_929bad8b21cea22",
 }
+
+// mysql://bfe3b37575de05:c82fda53@eu-cluster-west-01.k8s.cleardb.net/heroku_929bad8b21cea22?reconnect=true
